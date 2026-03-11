07e914e440e87c0c89811f6a5b6c60ee4f3cd4b6
scanalesR <scanales@rocketsoftware.com>
2026-03-11 11:28:48 -0500

    Update imports to use junit

--- backend/rings/src/test/java/com/goldeneye/rings/RingsApplicationTests.java
+++ backend/rings/src/test/java/com/goldeneye/rings/RingsApplicationTests.java
@@ -1,10 +1,8 @@
 package com.goldeneye.rings;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
-class RingsApplicationTests {
+public class RingsApplicationTests {
 
 	@Test
 	void contextLoads() {
