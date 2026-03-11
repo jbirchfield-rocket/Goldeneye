dd5ce542bab3b768c8d0740dc1d514a3d3b0eaee
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2026-03-11 09:28:35 -0500

    Deleted deleteme. Updated index.html so that the eye logo is in the tab as the Icon and Goldeneye is the name of the tab. Updated index.ts and App.vue to use router-link and router-view to replace the content of App.vue with the desired content

--- frontend/rings/src/router/index.ts
+++ frontend/rings/src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue';
+
+
+const paths = [
+  { path: '/', component: Home },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: paths,
 })
 
 export default router
--- frontend/rings/src/views/Home.vue
+++ frontend/rings/src/views/Home.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <!-- <h1>You did it!</h1>
+  <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
-  </p> -->
-  <div>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
-  <router-view />
-  </div>
+  </p>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
