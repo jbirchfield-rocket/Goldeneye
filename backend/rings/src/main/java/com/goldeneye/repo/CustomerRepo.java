842eae05701818a73cfa6a60e9aee34112d47f67
Dennis Shelby <dshelby@rocketsoftware.com>
2026-03-12 13:05:49 -0500

    Created CustomerRepo interface.

--- backend/rings/src/main/java/com/goldeneye/repo/CustomerRepo.java
+++ backend/rings/src/main/java/com/goldeneye/repo/CustomerRepo.java
@@ -5,10 +5,14 @@
 
 package com.goldeneye.repo;
 
+import org.springframework.data.repository.ListCrudRepository;
+
+import com.goldeneye.model.Customer;
+
+
 /**
  *
  * @author scanales
  */
-public interface CustomerRepo {
-
+public interface CustomerRepo extends ListCrudRepository<Customer, Integer> {
 }
