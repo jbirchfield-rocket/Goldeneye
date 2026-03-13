07b79098aa98382ba1fd17e7d0994139c21de4bd
scanalesR <scanales@rocketsoftware.com>
2026-03-13 10:23:36 -0500

    Implement Integration Tests for LocationService

---
+++ backend/rings/src/test/java/com/goldeneye/service/LocationServiceTests.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+package com.goldeneye.service;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.goldeneye.rings.dto.LocationDTO;
+import com.goldeneye.rings.RingsApplication;
+ 
+/**
+ *
+ * @author scanales
+ */
+
+@SpringBootTest(classes = RingsApplication.class)
+@DisplayName("Location Service Tests")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Tag("integration")
+public class LocationServiceTests {
+
+    @Autowired
+    private LocationService locationService;
+
+    @Test
+    void getAllLocationsReturnsListOfLocationDTOs() {
+        List<LocationDTO> locations = locationService.getAllLocations();
+        assertNotNull(locations);
+        assertFalse(locations.isEmpty());
+        assertTrue(locations.stream().allMatch(l -> l instanceof LocationDTO));
+        assertEquals(50, locations.size());
+        assertTrue(locations.stream().anyMatch(l -> l.getCustId() == 2 && l.getStreet().equals("123 Main St.") && l.getCity().equals("Richmond") && l.getState().equals("VA")));
+        assertTrue(locations.stream().anyMatch(l -> l.getCustId() == 12 && l.getStreet().equals("202 Maple Ln") && l.getCity().equals("Hicksville") && l.getState().equals("NY")));
+    }
+
+    @Test
+    void getLocationsByCustomerIdReturnsCorrectLocations() {
+        List<LocationDTO> locations = locationService.getLocationsByCustomerId(2);
+        assertNotNull(locations);
+        assertFalse(locations.isEmpty());
+        assertTrue(locations.stream().allMatch(l -> l instanceof LocationDTO));
+        assertEquals(4, locations.size());
+        assertTrue(locations.stream().anyMatch(l -> l.getCustId() == 2 && l.getStreet().equals("123 Main St.") && l.getCity().equals("Richmond") && l.getState().equals("VA")));
+        assertTrue(locations.stream().anyMatch(l -> l.getCustId() == 2 && l.getStreet().equals("789 Oak Ave") && l.getCity().equals("Austin") && l.getState().equals("TX")));
+    }
+}
