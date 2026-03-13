7a11da9c84aa226d99762614d2ebd130b0f4ad2c
Dennis Shelby <dshelby@rocketsoftware.com>
2026-03-13 14:30:51 -0500

    Implemented ProductDTO

--- backend/rings/src/main/java/com/goldeneye/dto/ProductDTO.java
+++ backend/rings/src/main/java/com/goldeneye/dto/ProductDTO.java
@@ -1,1 +1,41 @@
+package com.goldeneye.dto;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author dshelby
+ */
+
+public class ProductDTO {
+    private int prodId;
+    private String name;
+    private String description;
+    private BigDecimal basePrice;
+
+    public ProductDTO(int custId, String name) {
+        this.prodId = prodId;
+        this.name = name;
+        this.description = description;
+        this.basePrice = basePrice;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getBasePrice() {
+        return basePrice;
+    }
+
+
+}
 
