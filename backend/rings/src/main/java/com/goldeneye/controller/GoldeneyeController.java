71435f05567b490a408e3ca54f0b639777c92094
Dennis Shelby <dshelby@rocketsoftware.com>
2026-03-13 09:18:36 -0500

    Added @CrossOrigin to controller.

--- backend/rings/src/main/java/com/goldeneye/controller/GoldeneyeController.java
+++ backend/rings/src/main/java/com/goldeneye/controller/GoldeneyeController.java
@@ -8,6 +8,7 @@ package com.goldeneye.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import com.goldeneye.service.CustomerService;
  * @author dshelby
  */
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class GoldeneyeController {
 
