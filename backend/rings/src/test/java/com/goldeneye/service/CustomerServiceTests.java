cb45e056ba2fd5d37d8e3e1e0a55a1ecfc4712d4
scanalesR <scanales@rocketsoftware.com>
2026-03-13 09:32:44 -0500

    Tag Integration Tests to be Skipped in Workflow

--- backend/rings/src/test/java/com/goldeneye/service/CustomerServiceTests.java
+++ backend/rings/src/test/java/com/goldeneye/service/CustomerServiceTests.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +30,7 @@ import com.goldeneye.rings.RingsApplication;
 @SpringBootTest(classes = RingsApplication.class)
 @DisplayName("Customer Service Tests")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Tag("integration")
 public class CustomerServiceTests {
 
     @Autowired
