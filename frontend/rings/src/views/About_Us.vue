7ff740cd6ebd46ef472e9210a8b6703b13e6de8c
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2026-03-16 11:14:56 -0500

    Changed the About Us page to have unique text (not done)

--- frontend/rings/src/views/About_Us.vue
+++ frontend/rings/src/views/About_Us.vue
@@ -2,11 +2,19 @@
 
 <template>
   <h1>About Us</h1>
+
+  <h2>What we do:</h2>
   <p>
-    Goldeneye is a company dedicated to those who have an eye for golden perfection.
-     We sell only the highest quality rings at reasonable prices. 
-     Have gold in your eyes and on your fingers.
+    Goldeneye crafts high-quality, customizable rings that allow customers to express how they truly
+    feel. We hope to provide a memento that looks as good as it feels. 
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+  font-size: 1.1em;
+  line-height: 1.6;
+  padding: 10px 0;
+  max-width: 800px;
+}
+</style>
