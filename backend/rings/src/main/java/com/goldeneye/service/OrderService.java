6cd5aff7ed5e4390447a9cf16c7fb13bfadc4789
scanalesR <scanales@rocketsoftware.com>
2026-03-16 16:09:58 -0500

    Begin Implementation of calculateUnitPrice() Method in OrderService

--- backend/rings/src/main/java/com/goldeneye/service/OrderService.java
+++ backend/rings/src/main/java/com/goldeneye/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.goldeneye.service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import org.springframework.stereotype.Service;
@@ -10,25 +11,35 @@ import com.goldeneye.dto.OrderItemDTO;
 import com.goldeneye.repo.OrderItemRepo;
 import com.goldeneye.repo.OrderRepo;
 
+
 /**
  *
- * @author dshelby
+ * @author dshelby, scanalesR
  */
 @Service
 public class OrderService {
 
     private final OrderRepo orderRepo;
     private final OrderItemRepo orderItemRepo;
+    private final ProductService productService;
+    private final StoneService stoneService;
+    private final MaterialService materialService;
+    private final WidthService widthService;
 
-    public OrderService(OrderRepo orderRepo, OrderItemRepo orderItemRepo) {
+    public OrderService(OrderRepo orderRepo, OrderItemRepo orderItemRepo, ProductService productService, StoneService stoneService, MaterialService materialService, WidthService widthService) {
         this.orderRepo = orderRepo;
         this.orderItemRepo = orderItemRepo;
+        this.productService = productService;
+        this.stoneService = stoneService;
+        this.materialService = materialService;
+        this.widthService = widthService;
     }
 
     @Transactional
     public int createOrder(OrderDTO orderDTO) {
         orderRepo.insertOrder(orderDTO.getCustId(), orderDTO.getLocationId());
         int orderId = orderRepo.getLastGeneratedId();
+
 
         for (OrderItemDTO dto : orderDTO.getOrderItems()) {
             orderItemRepo.insertOrderItem(
@@ -37,11 +48,21 @@ public class OrderService {
                 dto.getMaterialId(),
                 dto.getWidthId(),
                 dto.getStoneId(),
-                null, // TODO: populate once calculateUnitPrice() is implemented
+                calculateUnitPrice(dto.getProductId(), dto.getMaterialId(), dto.getWidthId(), dto.getStoneId(), dto.getQuantity()),
                 dto.getQuantity()
             );
         }
 
         return orderId;
+    }
+
+    public BigDecimal calculateUnitPrice(int productId, int materialId, int widthId, int stoneId, int quantity) {
+        // get base price from product
+        // get stone price from stone
+        // get material multiplier from material
+        // get width multiplier from width
+        // calculate final price: (stone price + (base price * material multiplier * width multiplier)) * quantity
+        
+        return BigDecimal.ZERO;
     }
 }
