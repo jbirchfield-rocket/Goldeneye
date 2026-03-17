2ba02fda0d13107338a71b9b8a32723ea2e8a760
Dennis Shelby <dshelby@rocketsoftware.com>
2026-03-17 16:30:47 -0500

    Updated repos and OrderService to handle get orders.

--- backend/rings/src/main/java/com/goldeneye/repo/OrderItemRepo.java
+++ backend/rings/src/main/java/com/goldeneye/repo/OrderItemRepo.java
@@ -1,6 +1,7 @@
 package com.goldeneye.repo;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
@@ -30,4 +31,7 @@ public interface OrderItemRepo extends ListCrudRepository<OrderItem, Integer> {
         @Param("unitPrice") BigDecimal unitPrice,
         @Param("qty") int quantity
     );
+
+    @Query("SELECT OI.ORDITMID, OI.ORDERID, P.NAME AS PRODNAME, M.NAME AS MATTNAME, W.WIDTH, S.NAME AS STONENAME, OI.UNITPRICE, OI.QTY FROM GLDEYE.TBORDITM OI JOIN GLDEYE.TBPROD P ON P.PRODID = OI.PRODID JOIN GLDEYE.TBMATT M ON M.MATTID = OI.MATTID JOIN GLDEYE.TBWIDTH W ON W.WID = OI.WID JOIN GLDEYE.TBSTONE S ON S.STONEID = OI.STONEID WHERE OI.ORDERID = :orderId")
+    List<OrderItemSummaryRow> findOrderItemSummariesByOrderId(@Param("orderId") int orderId);
 }
