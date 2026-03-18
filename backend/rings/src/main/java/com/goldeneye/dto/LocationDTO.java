d46a784fd16f4389f7b5cae8ea4c7b30affc4fac
scanalesR <scanales@rocketsoftware.com>
2026-03-18 14:31:42 -0500

    Add setters to location dto and update ids to Integers

--- backend/rings/src/main/java/com/goldeneye/dto/LocationDTO.java
+++ backend/rings/src/main/java/com/goldeneye/dto/LocationDTO.java
@@ -6,8 +6,8 @@ package com.goldeneye.dto;
  */
 
 public class LocationDTO {
-    private int locId;
-    private int custID;
+    private Integer locId;
+    private Integer custId;
     private String street;
     private String city;
     private String state;
@@ -16,39 +16,61 @@ public class LocationDTO {
     public LocationDTO() {
     }
 
-    public LocationDTO(int locId, int custID, String street, String city, String state, String zip) {
+    public LocationDTO(Integer locId, Integer custId, String street, String city, String state, String zip) {
         this.locId = locId;
-        this.custID = custID;
+        this.custId = custId;
         this.street = street;
         this.city = city;
         this.state = state;
         this.zip = zip;
     }
 
-    public int getLocID() {
+    public Integer getLocId() {
         return locId;
     }
+
+    public void setLocId(Integer locId) {
+        this.locId = locId;
+    }
     
-    public int getCustID() {
-        return custID;
+    public Integer getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Integer custId) {
+        this.custId = custId;
     }
 
     public String getStreet() {
         return street;
     }
 
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
     public String getCity() {
         return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public String getState() {
         return state;
     }
 
+    public void setState(String state) {
+        this.state = state;
+    }
+
     public String getZip() {
         return zip;
     }
 
-
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
 
