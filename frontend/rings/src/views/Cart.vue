f33440e2114b171e240329758922c6eedba1c710
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2026-03-19 14:32:11 -0500

    State postal code must be uppercase now

--- frontend/rings/src/views/Cart.vue
+++ frontend/rings/src/views/Cart.vue
@@ -299,7 +299,7 @@ onMounted(() => {
           <form @submit.prevent="handleAddLocation">
             <input class="location-input" v-model="newLocation.street" placeholder="Street" required />
             <input class="location-input" v-model="newLocation.city" placeholder="City" required />
-            <input class="location-input" v-model="newLocation.state" placeholder="State (Postal Code e.g. CA)" maxlength="2" required />
+            <input class="location-input" v-model="newLocation.state" placeholder="State (Postal Code e.g. CA)" maxlength="2" pattern="[A-Z]{2}" required />
             <input class="location-input" v-model="newLocation.zip" placeholder="ZIP Code" maxlength="5" pattern="[0-9]*" required />
             <button class="add-location-btn" type="submit">Add Location</button>
           </form>
